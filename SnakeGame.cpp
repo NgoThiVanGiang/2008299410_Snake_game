c7b2a519b3c126ea5efe0aac38eac8cf4924aa1c
Nguyen Duc Y <nguyenducy003@gmail.com>
2025-08-19 14:43:46 +0700

    add generateFood

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -53,6 +53,10 @@ void SnakeGame::selectDifficulty() {
     _getch();
 }
 
+void SnakeGame::generateFood() {
+    WIDTH - 2;
+}
+
 void SnakeGame::handleInput() {
     if (_kbhit()) {
         int key = _getch();
